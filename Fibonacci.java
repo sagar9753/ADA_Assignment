334fde991855ede949114ff5d9458f4479897406
Sagar Rajput <sagar.rajput9753@gmail.com>
2023-05-13 11:48:21 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,20 @@
+class Fibo {
+    static int fib(int n)
+    {
+        // Base Case
+        if (n <= 1)
+            return n;
+
+        return fib(n - 1) + fib(n - 2);
+    }
+  
+    public static void main(String args[])
+    {
+        int N = 10;
+ 
+        for (int i = 0; i < N; i++) {
+ 
+            System.out.print(fib(i) + " ");
+        }
+    }
+}
